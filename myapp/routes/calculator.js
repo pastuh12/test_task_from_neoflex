0abbaa836f2f6a1ee810212874babcabfa55fb03
Tarasov ILya <ilyatarasov75@gmail.com>
2022-11-02 05:56:25 +0300

    add all routes for calculator

--- myapp/routes/calculator.js
+++ myapp/routes/calculator.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-var calculator_controller = require('../controllers/сalculatorController');
+var numberParser = require('../middleware/numberParser');
+var checkForInfinity = require('../middleware/checkForInfinity');
+var calculatorController = require('../controllers/сalculatorController');
 
-/* GET users listing. */
-router.get('/sum', calculator_controller.sum);
+/* GET calculator listing. */
+router.get('/sum', numberParser.parser, calculatorController.sum);
+router.get('/minus', numberParser.parser, calculatorController.minus);
+router.get('/multiplication', numberParser.parser, calculatorController.multi);
+router.get('/division', numberParser.parser, checkForInfinity.check,calculatorController.division);
+
 
 module.exports = router;
