ca144d8e498723df58307a669acb423a1220c83b
Tarasov ILya <ilyatarasov75@gmail.com>
2022-11-02 05:58:12 +0300

    edit require of numberErrorsHandler

--- myapp/app.js
+++ myapp/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var errorsHandler = require('./middleware/errorsController')
 
 var indexRouter = require('./routes/index');
 var calculatorRouter = require('./routes/calculator');
@@ -27,7 +28,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+// errors handler
+app.use(errorsHandler.ErrorsHandler)
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
